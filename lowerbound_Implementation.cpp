a11ab8a3d352a9420164284541a63764cf3fece6
Seunghwan <114864057+Seunghwan8334@users.noreply.github.com>
2024-07-03 18:23:56 +0900

    Add files via upload

---
+++ lowerbound_Implementation.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int left,right,num;
+    scanf("%d %d %d",&left, &right, &num);
+
+    while(left < right) {
+        printf("left = %d right = %d\n",left,right);
+        int mid = (left+right)/2;
+        if (mid < num) left = mid+1;
+        else right = mid;
+    }
+    // right 값이 lowerbound가 된다
+    printf("%d",right);
+}
